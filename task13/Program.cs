8286c73d917e463687acf9d38a36ed86723d68c9
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-24 21:53:34 +0600

    tasks 13, 15 complete

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    while (num > 999)
+    {
+    num /= 10;
+    }
+    num %= 10;
+    Console.Write(num);
+}
---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+while (day < 1 || day > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите номер дня недели: ");
+    day = Convert.ToInt32(Console.ReadLine());    
+}
+
+if (day <= 5)
+{
+    Console.WriteLine("Рабочий");
+}
+
+else
+{
+    Console.WriteLine("Выходной");
+}
