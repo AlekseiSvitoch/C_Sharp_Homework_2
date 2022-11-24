bf3ed6b42bc7b405ebd91c46bfa697377cc4c9a0
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-24 17:11:07 +0600

    added text of task 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Clear();
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+Console.Clear();
 Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.Write((num / 10) % 10);
